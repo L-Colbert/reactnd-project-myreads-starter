3822e4384bf35bcb23fbed5127c6ebd161f8b652
Libby Colbert <colbert.libby@gmail.com>
2018-09-18 23:18:38 -0400

    add Books component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-
+import Books from './Books'
 
 class BookShelf extends Component {
     render() {
         return (
             <div className="bookshelf">
                 <h2 className="bookshelf-title">Currently Reading</h2>
+                    <Books />
                 <h2 className="bookshelf-title">Want to Read</h2>
                 <h2 className="bookshelf-title">Read</h2>
             </div>
