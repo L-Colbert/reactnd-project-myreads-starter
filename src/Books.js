75d1602bf0862a9bea3c15d7dab391fdbe07df88
Libby Colbert <colbert.libby@gmail.com>
2018-09-18 23:30:54 -0400

    add ChangeShelf component

--- src/Books.js
+++ src/Books.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
+import ChangeShelf from './ChangeShelf'
 
 class Books extends Component {
     render() {
@@ -11,6 +12,7 @@ class Books extends Component {
                         <div className="book">
                             <div className="book-top">
                                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                                <ChangeShelf />
                             </div>
                             <div className="book-title">To Kill a Mockingbird</div>
                             <div className="book-authors">Harper Lee</div>
@@ -18,9 +20,9 @@ class Books extends Component {
                     </li>
                 </ol>
             </div>
-                    )
-                }
-            
-            }
-            
+        )
+    }
+
+}
+
 export default Books
